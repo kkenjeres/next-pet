29af91fa25bf2330cadbfee28f37210cbe4131b1
kkenjeres <kkenjeres96@gmail.com>
2025-09-02 17:30:44 +0200

    add prettier

--- src/common/Hero/index.tsx
+++ src/common/Hero/index.tsx
@@ -2,7 +2,7 @@ import { Button } from "../ui/button";
 export const Hero = () => {
   return (
     <>
-      <Button>Hi</Button>
+      <Button> Hi</Button>
     </>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   darkMode: ["class"],
-  content: [
-    "./app/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
-  ],
+  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
   theme: {
     extend: {
       colors: {
